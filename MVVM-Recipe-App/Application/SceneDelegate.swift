e9b6533e6834faafb5180a5e76f9fb2881470f02
matthewdovey <matthewdovey@hotmail.co.uk>
2020-11-18 21:41:19 +0000

    creation and start or application coordinator in scene delegate

--- MVVM-Recipe-App/Application/SceneDelegate.swift
+++ MVVM-Recipe-App/Application/SceneDelegate.swift
@@ -10,13 +10,25 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var coordinator: Coordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let appWindow = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController()
+        
+        coordinator = AppCoordionator(
+            children: [],
+            navigationController: navigationController
+        )
+        
+        coordinator?.start()
+        
+        appWindow.rootViewController = navigationController
+        appWindow.makeKeyAndVisible()
+
+        window = appWindow
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,7 +58,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
