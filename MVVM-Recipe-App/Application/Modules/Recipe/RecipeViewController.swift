851e090822d66108bba5eda08b2e8dd865d724ca
matthewdovey <matthewdovey@hotmail.co.uk>
2020-11-25 21:01:49 +0000

    creation of recipe view controller

---
+++ MVVM-Recipe-App/Application/Modules/Recipe/RecipeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RecipeViewController.swift
+//  MVVM-Recipe-App
+//
+//  Created by Matthew Dovey on 25/11/2020.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
