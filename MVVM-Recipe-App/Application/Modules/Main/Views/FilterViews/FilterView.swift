de3da25f9a0b437ba6ca2cc7b0ddc569c54772ed
matthewdovey <matthewdovey@hotmail.co.uk>
2020-11-29 17:12:28 +0000

    initial creation of filter view

---
+++ MVVM-Recipe-App/Application/Modules/Main/Views/FilterViews/FilterView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterView.swift
+//  MVVM-Recipe-App
+//
+//  Created by Matthew Dovey on 29/11/2020.
+//
+
+import UIKit
+
+class FilterView: UIView {
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
