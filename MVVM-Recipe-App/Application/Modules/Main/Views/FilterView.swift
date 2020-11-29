1936a079951c4a4e8244dc4aa6cdff6671820434
matthewdovey <matthewdovey@hotmail.co.uk>
2020-11-29 15:45:52 +0000

    initial setup of filter view ui

--- MVVM-Recipe-App/Application/Modules/Main/Views/FilterView.swift
+++ MVVM-Recipe-App/Application/Modules/Main/Views/FilterView.swift
@@ -8,5 +8,32 @@
 import UIKit
 
 class FilterView: UIView {
+    private var filterStackView: UIStackView!
+    	
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setup()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        setupFilterStack()
+    }
+    
+    private func setupFilterStack() {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .trailing
+        filterStackView = stackView
+        
+        addSubview(filterStackView)
+        filterStackView.translatesAutoresizingMaskIntoConstraints = false
+        filterStackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        filterStackView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        filterStackView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        filterStackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
 }
